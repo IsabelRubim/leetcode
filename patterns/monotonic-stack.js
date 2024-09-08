929f3bd8b6b1684c4309ac88cd91694e63bcd8dc
Isabel Rubim <isabel_rubim@hotmail.com.br>
2024-09-08 23:17:15 +0000

    add pattern about monotonic stack

---
+++ patterns/monotonic-stack.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const answer = Array(temperatures.length).fill(0); 
+    const stack = [];
+
+    for (let i = 0; i < temperatures.length; i++) {
+        
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            answer[prevIndex] = i - prevIndex;
+        }
+
+        stack.push(i);
+    }
+
+    return answer;
+};
